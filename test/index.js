58d6516ade7b976881cd869e7fb4b8971c67d971
Jonathan Ong <jonathanrichardong@gmail.com>
2016-05-22 17:00:46 -0700

    test: setup for failure cases

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const exec = require('child_process').exec
+const assert = require('assert')
 const path = require('path')
 const fs = require('fs')
 
@@ -9,6 +10,17 @@ describe('eslint-config-jongleberry', () => {
     fs.readdirSync(path.join(__dirname, 'fixtures/pass')).forEach(file => {
       it(`pass/${file}`, done => {
         exec(`./node_modules/.bin/eslint test/fixtures/pass/${file}`, done)
+      })
+    })
+  })
+
+  describe('these files should not pass linting:', () => {
+    fs.readdirSync(path.join(__dirname, 'fixtures/fail')).forEach(file => {
+      it(`fail/${file}`, done => {
+        exec(`./node_modules/.bin/eslint test/fixtures/fail/${file}`, err => {
+          assert(err)
+          done()
+        })
       })
     })
   })
